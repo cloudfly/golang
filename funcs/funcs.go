7ece20b96d83d4e485551bfe16a197850525cacf
kuwu <kuwu@wacai.com>
2018-09-02 17:07:09 +0800

    增加字符串匹配函数

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,6 +1,7 @@
 package funcs
 
 import (
+	"bytes"
 	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
@@ -10,4 +11,26 @@ func Hash(data []byte) string {
 	s := sha1.Sum(data)
 	m := md5.Sum(data)
 	return fmt.Sprintf("%x%x", s[:8], m[:8])
+}
+
+func SimpleMatch(pattern, s string) bool {
+	if pattern == "" { // empty pattern only match empty string
+		return s == ""
+	}
+	patternBytes, name := []byte(pattern), []byte(s) // parse to []byte saving the memroy and reduce gc for string
+	items := bytes.Split(patternBytes, []byte{'*'})
+	for i, item := range items {
+		if i == len(items)-1 && len(item) == 0 { // pattern end with *
+			return true
+		}
+		j := bytes.Index(name, item)
+		if j == -1 {
+			return false
+		}
+		if i == 0 && len(item) != 0 && j != 0 { // 保证 abc* 匹配以 abc 开头的, 否则会匹配到 aabcxx 这种
+			return false
+		}
+		name = name[j+len(item):]
+	}
+	return len(name) == 0
 }
