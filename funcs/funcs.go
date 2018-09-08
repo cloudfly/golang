ab871ac0e7bd799da820e6e98c294055923438f6
kuwu <kuwu@wacai.com>
2018-09-08 08:06:33 +0800

    增加 rsa publick key 解密方法

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -3,8 +3,13 @@ package funcs
 import (
 	"bytes"
 	"crypto/md5"
+	"crypto/rsa"
 	"crypto/sha1"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"math/big"
 )
 
 func Hash(data []byte) string {
@@ -33,4 +38,36 @@ func SimpleMatch(pattern, s string) bool {
 		name = name[j+len(item):]
 	}
 	return len(name) == 0
+}
+
+func RSAPublickDecryptEasy(key, data []byte) ([]byte, error) {
+	block, _ := pem.Decode(key)
+	if block == nil || block.Type != "PUBLIC KEY" {
+		return nil, errors.New("failed to decode PEM block containing public key")
+	}
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse public key: %s", err.Error())
+	}
+	return RSAPublickDecrypt(pub.(*rsa.PublicKey), data), nil
+}
+
+func RSAPublickDecrypt(pubKey *rsa.PublicKey, data []byte) []byte {
+	c := new(big.Int)
+	m := new(big.Int)
+	m.SetBytes(data)
+	e := big.NewInt(int64(pubKey.E))
+	c.Exp(m, e, pubKey.N)
+	out := c.Bytes()
+	skip := 0
+	for i := 2; i < len(out); i++ {
+		if i+1 >= len(out) {
+			break
+		}
+		if out[i] == 0xff && out[i+1] == 0 {
+			skip = i + 2
+			break
+		}
+	}
+	return out[skip:]
 }
